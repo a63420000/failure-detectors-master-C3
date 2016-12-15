c9ace4d5fe5c127da20124dcaef30b7ba6859db3
a63420000 <sylphis00522@gmail.com>
2016-12-15 03:19:34 -0800

    20161215

---
+++ failure-detectors-master/src/main/java/com/hellblazer/utils/fd/impl/EndNode.java
@@ -0,0 +1,208 @@
+package com.hellblazer.utils.fd.impl;
+
+import java.io.*;
+import java.net.*;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.nio.ByteBuffer;
+import java.util.List;
+import java.util.ArrayList;
+import com.hellblazer.utils.fd.*;
+
+public class EndNode extends Thread{
+	
+	int port;
+	InetAddress server;
+	FailureDetectorFactory adapFDfac;
+	public static Map<Integer,Integer> gossipList = new ConcurrentHashMap<Integer,Integer>();
+	public static Map<Integer,FailureDetector> FDList = new ConcurrentHashMap<Integer,FailureDetector>();
+        public static Integer[][] suspectMtx = new Integer[4][4];
+	public static List<Integer> aliveList = new ArrayList<Integer>();
+        public static List<Integer> deadList = new ArrayList<Integer>();
+	public static long[] fctime = new long[4];
+	public static int[] fcflag = new int[4];
+	public static int fccount = 0;
+	public static long[] ft = new long[100];
+	//public static int[] ff = new int[100];
+	public static int index = 1;
+	public static int[] mc = new int[100];
+	
+	public EndNode(String pServer, int pPort) {
+		try {
+			port = pPort;
+			server = InetAddress.getByName(pServer);
+			adapFDfac = new AdaptiveFailureDetectorFactory(0.3, 1000, 1.0, 20000, 100, 500);
+			FailureDetector adapFD = adapFDfac.create();
+			FDList.put(port, adapFD);
+
+			aliveList.add(port);
+
+		} catch (Exception e) { e.printStackTrace(); }
+	}
+	
+	public void run(){
+		
+		try{
+			
+			ByteBuffer rvbuffer = ByteBuffer.allocateDirect(65507);
+			
+			InetSocketAddress client;
+			if((client = (InetSocketAddress)FdMain.channel.receive(rvbuffer)) != null)
+			{
+                                System.out.println("Receive Success");
+				System.out.println("Receive from: "+client.getPort());
+				rvbuffer.flip();
+				byte rvbufferarr[] = new byte[rvbuffer.remaining()];
+				rvbuffer.get(rvbufferarr);				
+				ByteArrayInputStream bais = new ByteArrayInputStream(rvbufferarr);
+				ObjectInputStream ois = new ObjectInputStream(bais);
+
+				Object rvobj;
+				if((rvobj = ois.readObject()) instanceof Map<?,?>)
+				{
+					Map<Integer,Integer> rvgl = (Map<Integer,Integer>)rvobj;
+					for(int i=0; i<4; i++)
+					{
+						int rvhc = rvgl.get(5554+i);
+						int cuhc = gossipList.get(5554+i);
+						if(rvhc > cuhc)
+						{
+							FDList.get(5554+i).record(System.currentTimeMillis(), 0L);
+							gossipList.put(5554+i, rvhc);
+							//suspectMtx[i][i] = 0;				
+						}
+						System.out.println("Receive 5554+"+i+": "+cuhc);
+					}
+				}
+				if((rvobj = ois.readObject()) instanceof Integer[][])
+				{
+					System.out.println("Receive Mtx");
+					Integer[][] rvsm = (Integer[][])rvobj;
+					for(int i=0; i<4; i++)
+					{						
+						if(i == FdMain.SendportList.get(client.getPort()))
+						{
+							for(int j=0; j<4; j++)
+							{
+								suspectMtx[i][j] = rvsm[i][j];
+							}							
+						}
+						else
+						{
+							if(i != FdMain.SendportList.get(5502))
+							{
+								for(int j=0; j<4; j++)
+								{
+									if(suspectMtx[i][j] == -1 && rvsm[i][j] == 1)
+										suspectMtx[i][j] = rvsm[i][j];
+									else
+										suspectMtx[i][j] = suspectMtx[i][j] | rvsm[i][j];
+								}
+							}
+						}
+					}					
+				}						
+			}
+			else
+				System.out.println("Receive Nothing");
+			
+			long now = System.currentTimeMillis();
+			for(int i=0; i<4; i++)
+			{
+				FailureDetector adapFD = FDList.get(5554+i);
+				if(adapFD != null)
+				{
+					if(!adapFD.shouldConvict(now)) {
+						fcflag[i] = 0;
+						System.out.println("5554+"+i+"-Alive-");
+						if(suspectMtx[2][i] == 1 || suspectMtx[2][i] == -1)
+							suspectMtx[2][i] = -1;
+						else
+							suspectMtx[2][i] = 0;
+						Integer p = new Integer(5554+i);
+						//Integer pi = new Integer(i);
+						if(!aliveList.contains(p))
+							aliveList.add(p);
+						//if(deadList.contains(pi))
+						//	deadList.remove(pi);
+                                        }
+					else {
+						
+						System.out.println("5554+"+i+"-Dead-");
+						suspectMtx[2][i] = 1;
+						System.out.println(suspectMtx[2][i]);
+
+						int dcount = 0;
+						//dcount +=deadList.size();
+						for(int j=0; j<4; j++)
+						{
+							if(suspectMtx[j][i] == 1)
+								dcount++;
+							if((dcount>aliveList.size()/2 || aliveList.size() == 2) && suspectMtx[i][i] == 0)
+							{
+								suspectMtx[i][i] = 1;
+								if(j>=i)
+								{
+									dcount++;
+								}
+							}
+							if(dcount == aliveList.size())
+							{
+								if(fcflag[i] == 0)
+								{
+									fctime[i] = System.currentTimeMillis();
+									fcflag[i] = 1;
+									fccount++;
+									if(i == 0 && gossipList.get(5554) > 1000 * index)
+									{
+										if(index == 1)
+										{
+											index++;
+										}
+										else
+										{	
+											ft[index-1] = fctime[i];
+											fccount -= 1;
+											int cmc = fccount;
+											for(int m=0; m<index-1; m++)
+												cmc-=mc[m];
+											mc[index-1] = cmc;
+											index++;
+										}
+									}
+								}
+								System.out.println("5554+"+i+"-Dead Consensus-");
+								System.out.println("Consensus time: "+fctime[i]);
+								//System.out.println("fc count: "+fccount);
+								Integer p = new Integer(5554+i);
+								Integer pi = new Integer(i);
+								aliveList.remove(p);
+								if(!deadList.contains(pi))
+									deadList.add(pi);
+								break;
+							}
+						}
+					}
+				}
+			}
+			System.out.println("fc count: "+fccount);
+
+			if(port != 5556)
+			{
+				ByteArrayOutputStream baos = new ByteArrayOutputStream();
+				ObjectOutputStream oos = new ObjectOutputStream(baos);
+				oos.writeObject(gossipList);
+                        	oos.writeObject(suspectMtx);
+				oos.flush();
+				byte buffer[] = baos.toByteArray();
+			
+				DatagramPacket packet = new DatagramPacket(buffer, buffer.length, server, port);
+				DatagramSocket socket = new DatagramSocket(5502);
+				socket.send(packet);
+
+				socket.close();
+			}
+		} catch (Exception e) { e.printStackTrace(); }
+	}
+	
+}
---
+++ failure-detectors-master/src/main/java/com/hellblazer/utils/fd/impl/FdMain.java
@@ -0,0 +1,150 @@
+package com.hellblazer.utils.fd.impl;
+
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.channels.DatagramChannel;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.io.*;
+
+public class FdMain {
+
+	public static EndNode node1 = new EndNode("192.168.199.135", 5554);
+	public static EndNode node2 = new EndNode("192.168.199.139", 5555);
+	public static EndNode node3 = new EndNode("192.168.199.141", 5556);
+	public static EndNode node4 = new EndNode("192.168.199.142", 5557);
+	public static Map<Integer,EndNode> EndnodeList = new ConcurrentHashMap<Integer,EndNode>();
+	public static Map<Integer,Integer> SendportList = new ConcurrentHashMap<Integer,Integer>();
+        public static DatagramChannel channel;
+	public static int sdflag = 0;
+	public static int count = 0;
+	
+	public static void main(String[] args) {
+	 
+		System.out.println("Hello World");
+		//int count = 0;
+		EndNode.gossipList.put(5554, 0);
+		EndNode.gossipList.put(5555, 0);
+		EndNode.gossipList.put(5556, 0);
+		EndNode.gossipList.put(5557, 0);
+		EndnodeList.put(5554,node1);
+		EndnodeList.put(5555,node2);
+		EndnodeList.put(5556,node3);
+		EndnodeList.put(5557,node4);
+		SendportList.put(5500, 0);
+		SendportList.put(5501, 1);
+		SendportList.put(5502, 2);
+		SendportList.put(5503, 3);
+		for(int i=0; i<4; i++)
+		{
+			for(int j=0; j<4; j++)
+			{
+				EndNode.suspectMtx[i][j] = 0;
+			}
+		}
+		for(int i=0; i<4; i++)
+		{
+			EndNode.fctime[i] = 0;
+			EndNode.fcflag[i] = 0;
+		}
+
+		for(int i=0; i<100; i++)
+		{
+			EndNode.ft[i] = 0;
+			//EndNode.ff[i] = 0;
+			EndNode.mc[i] = 0;
+		}		
+
+		try {
+                        Runtime.getRuntime().addShutdownHook(new ShutdownThread());
+			Thread.sleep(10000);
+			channel = DatagramChannel.open();
+			DatagramSocket rvsocket = channel.socket();
+			SocketAddress addr = new InetSocketAddress(5556);
+			rvsocket.bind(addr);
+			channel.configureBlocking(false);
+		} catch (Exception e) { e.printStackTrace(); }
+
+		while(true)
+		{
+			try {
+/*
+				String[] cmd1 = {"sh", "-c", "ps aux | grep karaf | grep Rl+"};
+				Process pl = Runtime.getRuntime().exec(cmd1);
+				pl.waitFor();
+				String l = "";
+				BufferedReader p_in = new BufferedReader(new InputStreamReader(pl.getInputStream()));
+				l = p_in.readLine();
+				
+				String[] cmd2 = {"sh", "-c", "ps aux | grep karaf | grep Sl+"};
+				Process pl2 = Runtime.getRuntime().exec(cmd2);
+				pl2.waitFor();
+				String l2 = "";
+				BufferedReader p_in2 = new BufferedReader(new InputStreamReader(pl2.getInputStream()));
+				l2 = p_in2.readLine();
+				
+				if((l.contains("java")) || (l2.contains("java")))
+				{
+*/
+					sdflag = 0;
+					count++;
+					System.out.println("Round: "+count);
+					int hc = EndNode.gossipList.get(5556);
+					hc++;
+					EndNode.gossipList.put(5556, hc);
+					EndnodeList.get(EndNode.aliveList.get((EndNode.aliveList.indexOf(5556)+1)%EndNode.aliveList.size())).run();
+					//EndNode.gossipList.put(5556, hc);
+					Thread.sleep(100);
+					EndnodeList.get(EndNode.aliveList.get((EndNode.aliveList.indexOf(5556)+2)%EndNode.aliveList.size())).run();
+					Thread.sleep(100);
+					sdflag = 1;
+					EndnodeList.get(EndNode.aliveList.get((EndNode.aliveList.indexOf(5556))%EndNode.aliveList.size())).run();
+					Thread.sleep(100);
+					EndnodeList.get(EndNode.aliveList.get((EndNode.aliveList.indexOf(5556))%EndNode.aliveList.size())).run();
+					Thread.sleep(100);
+					EndnodeList.get(EndNode.aliveList.get((EndNode.aliveList.indexOf(5556))%EndNode.aliveList.size())).run();
+					Thread.sleep(100);
+/*
+				}
+				else
+				{
+					System.out.println("---ODL Karaf Dead---");
+					//Runtime.getRuntime().exec("/home/skysky/karaf");
+					//System.out.println("---Restart ODL Karaf---");
+				}
+				p_in.close();
+				p_in2.close();
+*/
+				//for(int i=0; i<4; i++)
+				//{
+				//	for(int j=0 ;j<4; j++)
+				//	{
+				//		System.out.println(EndNode.suspectMtx[i][j]);
+				//	}
+				//}
+			} catch (Exception e) { e.printStackTrace(); }
+		}
+					
+	}
+	 
+}
+
+class ShutdownThread extends Thread {
+	public ShutdownThread() {
+		super();		
+	}
+	
+	public void run() {
+		for(int i=0; i<EndNode.index; i++)
+		{
+			System.out.println("Detected Time "+(i+1)+": "+EndNode.ft[i]);
+		}
+		for(int i=0; i<EndNode.index; i++)
+		{
+			System.out.println("Mistakes "+(i+1)+": "+EndNode.mc[i]);
+		}
+		System.out.println("Current Time: "+System.currentTimeMillis());		
+	}
+		
+}
